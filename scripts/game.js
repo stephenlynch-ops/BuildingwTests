5d1068475c3d0d5b72a65cc1b0d6429ef0516937
Stephen Lynch <stephen.lynch17@icloud.com>
2022-06-18 18:19:24 +0000

    Added showTurns function

--- scripts/game.js
+++ scripts/game.js
@@ -10,10 +10,35 @@ function newGame() {
     game.playerMoves = [];
     game.score = 0;
     showScore();
-}
+    addTurn();
+};
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
+};
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+};
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ),classList.remove("light");
+    }, 400);
 }
 
-module.exports = { game, newGame, showScore };
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if(game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,8 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore } = require("../game");
-
+ const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
  beforeAll(() => {
      let fs = require("fs");
@@ -48,7 +47,35 @@
      test("should clear the player moves array", () => {
          expect(game.playerMoves.length).toBe(0);
      });
-     test("should clear the computer sequence array", () => {
-         expect(game.currentGame.length).toBe(0);
+     test("should add one move to the computer's game array", () => {
+         expect(game.currentGame.length).toBe(1);
      });
+ });
+
+ describe("game plays works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
  });
